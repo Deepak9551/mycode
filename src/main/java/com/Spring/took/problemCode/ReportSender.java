063aa278fdf7d9541b3ed2c54a5f1b954e1c6625
Deepak9551 <12pcm.deepak@gmail.com>
2025-02-14 22:49:26 +0530

    problem_report

---
+++ src/main/java/com/Spring/took/problemCode/ReportSender.java
@@ -0,0 +1,13 @@
+package com.Spring.took.problemCode;
+
+public class ReportSender {
+    public void SendReport(String s){
+        if (s.equals("PDF")){
+            System.out.println("this is pdf report");
+        } else if (s.equals("DOC")) {
+            System.out.println("this is doc report");
+        } else if (s.equals("EXCEL")) {
+            System.out.println("this is excel report");
+        }
+    }
+}
