01c56222107b87edc44e59a99c8a4958077f37e9
Deepak9551 <12pcm.deepak@gmail.com>
2025-02-15 22:02:06 +0530

    problemcode_CreditCard

---
+++ src/main/java/com/Spring/took/CreditCard/problemCode/Rupay.java
@@ -0,0 +1,23 @@
+package com.Spring.took.CreditCard.problemCode;
+
+public class Rupay extends CreditCard {
+    @Override
+    public void tapandpay() {
+        System.out.println("Tap and pay of RU_PAY");
+    }
+
+    @Override
+    public void onlineTransfer() {
+        System.out.println("Online Transfer of RU_PAY");
+    }
+
+    @Override
+    public void swipeAndPlay() {
+        System.out.println("Swipe and Play of RU_PAY");
+    }
+
+    @Override
+    public void mandatePayments() {
+        System.out.println("mandatePayments of RU_PAY");
+    }
+}
---
+++ src/main/java/com/Spring/took/CreditCard/problemCode/VisaCard.java
@@ -0,0 +1,23 @@
+package com.Spring.took.CreditCard.problemCode;
+
+public class VisaCard extends CreditCard {
+    @Override
+    public void tapandpay() {
+        System.out.println("Tap and pay of VISA");
+    }
+
+    @Override
+    public void onlineTransfer() {
+        System.out.println("Online Transfer of VISA");
+    }
+
+    @Override
+    public void swipeAndPlay() {
+        System.out.println("Swipe and Play of VISA");
+    }
+
+    @Override
+    public void mandatePayments() {
+        System.out.println("Mandate Payment of VISA");
+    }
+}
